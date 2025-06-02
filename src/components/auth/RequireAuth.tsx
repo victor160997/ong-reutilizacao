a58d3923e8e73fd3ea49ed20f3e82a7dbe801584
victor160997 <victor.men.dev@gmail.com>
2025-06-02 07:37:37 -0300

    refactor: updating require auth component to use currentUser from auth context

--- src/components/auth/RequireAuth.tsx
+++ src/components/auth/RequireAuth.tsx
@@ -1,30 +1,17 @@
-
-import { auth } from "@/lib/firebase";
-import { onAuthStateChanged, User } from "firebase/auth";
-import { useEffect, useState } from "react";
 import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "@/contexts/AuthContext";
 
 interface RequireAuthProps {
   children: JSX.Element;
 }
 
 export const RequireAuth = ({ children }: RequireAuthProps) => {
-
+  const { currentUser, loading } = useAuth();
   const location = useLocation();
 
-  const [currentUser, setCurrentUser] = useState<User | null>(null);
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setCurrentUser(user);
-      } else {
-        setCurrentUser(null);
-      }
-    });
-
-    return unsubscribe;
-  }, []);
+  if (loading) {
+    return <div className="h-screen flex items-center justify-center">Carregando...</div>;
+  }
 
   if (!currentUser) {
     return <Navigate to="/login" state={{ from: location }} replace />;
